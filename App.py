809006938ea4a4c42da5afbfbacb19f5e0979cc2
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-03-25 17:19:15 +0530

    Update App.py

--- App.py
+++ App.py
@@ -4,17 +4,15 @@ import numpy as np
 from PIL import Image
 import tensorflow as tf
 import requests
-from io import BytesIO
 
-# Load pre-trained models
-leaf_model = tf.keras.models.load_model('v3_pred_cott_dis.h5')
-img_model = tf.keras.applications.ResNet50(weights='imagenet')
+# Load pre-trained model
+model = tf.keras.models.load_model('v3_pred_cott_dis.h5')
 
 # Define labels for prediction output
-leaf_labels = ['diseased','healthy']
+labels = ['diseased','healthy']
 
-# Define function to preprocess input image for leaf model
-def preprocess_leaf_image(image):
+# Define function to preprocess input image
+def preprocess_image(image):
     # Resize image
     image = image.resize((150,150))
     # Convert image to numpy array
@@ -25,37 +23,14 @@ def preprocess_leaf_image(image):
     image = np.expand_dims(image, axis=0)
     return image
 
-# Define function to preprocess input image for img model
-def preprocess_img(image):
-    # Resize image
-    image = image.resize((224,224))
-    # Convert image to numpy array
-    image = np.array(image)
-    # Expand dimensions to create batch of size 1
-    image = np.expand_dims(image, axis=0)
-    # Preprocess image
-    image = tf.keras.applications.resnet50.preprocess_input(image)
-    return image
-
-# Define function to classify image using pre-trained img model
-def classify_image(image):
-    # Preprocess input image
-    image = preprocess_img(image)
-    # Make prediction using pre-trained model
-    prediction = img_model.predict(image)
-    # Get predicted class index
-    predicted_class = tf.keras.applications.resnet50.decode_predictions(prediction, top=1)[0][0][1]
-    # Return predicted class
-    return predicted_class
-
-# Define function to make prediction on input image using leaf model
+# Define function to make prediction on input image
 def predict(image):
     # Preprocess input image
-    image = preprocess_leaf_image(image)
+    image = preprocess_image(image)
     # Make prediction using pre-trained model
-    prediction = leaf_model.predict(image)
+    prediction = model.predict(image)
     # Convert prediction from probabilities to label
-    label = leaf_labels[np.argmax(prediction)]
+    label = labels[np.argmax(prediction)]
     # Return label and confidence score
     return label, prediction[0][np.argmax(prediction)]
 
@@ -69,26 +44,36 @@ def main():
     uploaded_file = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
     # If file uploaded, display it and make prediction
     if uploaded_file is not None:
-        # Load image
-        image = Image.open(uploaded_file)
-        # Check if the image is actually a cotton plant
-        predicted_class = classify_image(image)
-        if 'cotton' not in predicted_class:
-            st.write("Error: Please upload an appropriate image of a cotton plant.")
-        else:
-            # Display image
-            st.image(image, caption='Uploaded Image', use_column_width=True)
-            # Make prediction
-            label, score = predict(image)
-            # Display prediction
-            st.write('Prediction: {} (confidence score: {:.2f})'.format(label, score))
-        
-            # Provide instructions based on prediction
-            if label == 'diseased':
-                st.write('Your cotton plant appears to be diseased. To prevent the spread of disease, you should remove the infected plant and treat the soil. You can also consult a local agricultural expert for advice on how to prevent future outbreaks of disease.')
+        try:
+            # Load image
+            image = Image.open(uploaded_file)
+            # Check whether the image is a cotton plant image
+            response = requests.post(
+                "https://api.deepai.org/api/image-similarity",
+                data={
+                    'image': image,
+                    'key': 'YOUR_API_KEY'
+                }
+            )
+            result = response.json()
+            if result['output'][0]['similarity'] >= 0.5:
+                # Display image
+                st.image(image, caption='Uploaded Image', use_column_width=True)
+                # Make prediction
+                label, score = predict(image)
+                # Display prediction
+                st.write('Prediction: {} (confidence score: {:.2f})'.format(label, score))
+                
+                # Provide instructions based on prediction
+                if label == 'diseased':
+                    st.write('Your cotton plant appears to be diseased. To prevent the spread of disease, you should remove the infected plant and treat the soil. You can also consult a local agricultural expert for advice on how to prevent future outbreaks of disease.')
+                else:
+                    st.write('Your cotton plant appears to be healthy. To keep it healthy, make sure to provide adequate water and fertilize regularly. You should also control pests and prune and train the plant to promote healthy growth. Harvest at the right time to ensure the highest quality fiber.')
             else:
-                st.write('Your cotton plant appears to be healthy. To keep it healthy, make sure to provide adequate water and fertilize regularly. You should also control pests and prune and train the plant to promote healthy growth. Harvest at the right time to ensure the highest quality fiber.')
-
+                st.write('The uploaded image does not seem to be a cotton plant image. Please upload an appropriate image.')
+        except:
+            st.write('Error: Please try again with an appropriate image.')
+            
 # Run Streamlit app
 if __name__ == '__main__':
     main()

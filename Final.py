43cb811bb1bc8653fe7284383d795bce58e29f1f
Niraj-Aware <92052184+Niraj-Aware@users.noreply.github.com>
2023-05-02 20:14:43 +0530

    Update Final.py

--- Final.py
+++ Final.py
@@ -3,12 +3,14 @@ import streamlit as st
 # Define the function for the Farmer button
 def farmer():
     st.markdown("Redirecting to the Farmer app...")
-    st.redirect("https://niraj-aware-cpdp-project--cpdpapp-vpt5d7.streamlit.app/")
+    st.experimental_set_query_params(option="farmer")
+    st.experimental_redirect("https://niraj-aware-cpdp-project--cpdpapp-vpt5d7.streamlit.app/")
 
 # Define the function for the Pharmacist button
 def pharmacist():
     st.markdown("Redirecting to the Pharmacist app...")
-    st.redirect("https://niraj-aware-cpdp-project--app-y9w0bf.streamlit.app/")
+    st.experimental_set_query_params(option="pharmacist")
+    st.experimental_redirect("https://niraj-aware-cpdp-project--app-y9w0bf.streamlit.app/")
 
 # Define the Streamlit app
 def main():
@@ -25,4 +27,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
 
